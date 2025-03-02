96d54c89b47371b2573e9f3cc19d7a9181b0c60e
Sahil Sinnarkar <sahilsinnarkar@gmail.com>
2025-03-02 12:49:44 +0530

    removed a file

--- backend/nasmDemo.js
+++
@@ -1,9 +0,0 @@
-import { exec } from "child_process";
-
-exec("nasm -v", (error, stdout, stderr) => {
-    if (error) {
-        console.error("❌ Error:", error.message);
-        return;
-    }
-    console.log("✅ NASM Version:", stdout);
-});
